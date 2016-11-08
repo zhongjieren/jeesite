95d277e86563d7b2795abaed254328518729e0f1
arenzhj <arenzhj@163.com>
2016-11-08 17:29:06 +0800

    Init

--- src/main/java/com/thinkgem/jeesite/modules/sys/security/SystemAuthorizingRealm.java
+++ src/main/java/com/thinkgem/jeesite/modules/sys/security/SystemAuthorizingRealm.java
@@ -282,4 +282,5 @@ public class SystemAuthorizingRealm extends AuthorizingRealm {
 		}
 
 	}
+	 
 }
--- src/test/java/com/thinkgem/jeesite/test/Test.java
+++ src/test/java/com/thinkgem/jeesite/test/Test.java
@@ -1,8 +1,22 @@
 package com.thinkgem.jeesite.test;
 
+import com.thinkgem.jeesite.modules.sys.service.SystemService;
+
 public class Test {
 
 	public static void main(String[] args) {
-		
+		/**
+		 * 重新生成新密码
+		 */
+		getEntryptPwd();
+	}
+	
+	/**
+	 * 重新生成新密码
+	 */
+	public static void getEntryptPwd(){
+		String newPwd ="123456";
+		String enNewPwd = SystemService.entryptPassword(newPwd);
+		System.out.println(enNewPwd);
 	}
 }
